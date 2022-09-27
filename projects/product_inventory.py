ab1e48a1c0be9c3e47fefbedd86bab013cae5c08
Daniel Romero <danieljromero305@gmail.com>
2022-09-27 09:49:11 -0400

    Implemented basic loop

--- projects/product_inventory.py
+++ projects/product_inventory.py
@@ -3,3 +3,13 @@
 # Daniel Romero
 # CSCI 1301-HY8, Fall 2022
 # Dr. Abi Salimi
+
+product_list = []
+price_list = []
+
+for x in range(0,4):
+    tempProductInfo = input("Name of product " + str(x+1) + ": ")
+    tempProductPrice = input("Price of the product: ")
+    # I would use a switch statement but I'm worried about zybooks's ability to handle it
+    print(tempProductInfo)
+    print(tempProductPrice)
