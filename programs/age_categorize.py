bc95697311959d8c29999b672b3e08d8f825df60
Daniel Romero <danieljromero305@gmail.com>
2022-10-13 10:26:07 -0400

    Put program into while loop to print all vals

--- programs/age_categorize.py
+++ programs/age_categorize.py
@@ -1,12 +1,18 @@
 # 10/13/2022
 
-age = int(input("Enter an age: "))
+#age = int(input("Enter an age: "))
+age = 0
 
-if 0 < age <= 12:
-    print(age,"is Youth")
-elif age < 20:
-    print(age,"is Teen")
-elif age >= 25:
-    print(age,"is Adult")
-else:
-    print(age,"is invalid")
+def agePrint():
+    if 0 < age <= 12:
+        print(age,"is Youth")
+    elif 13 <= age < 20:
+        print(age,"is Teen")
+    elif age >= 25:
+        print(age,"is Adult")
+    else:
+        print(age,"is invalid")
+
+while age < 30:
+    agePrint()
+    age += 1
