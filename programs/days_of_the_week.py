132d978064e33d4455646d649041847edf43486e
Daniel Romero <danieljromero305@gmail.com>
2022-10-18 10:42:22 -0400

    changed comments and added prompt

--- programs/days_of_the_week.py
+++ programs/days_of_the_week.py
@@ -1,1 +1,4 @@
-#what was the prompt for this one help
+# Input a day of the week from Sunday to Saturday. 
+# Determine if that day is a work-day or a weekend.
+# Repeat the process until the user enters “stop”
+
