241597799b10c14df1c458a63598ea7ba5e67512
Daniel Romero <danieljromero305@gmail.com>
2022-10-18 10:34:14 -0400

    Created number_increase.py

---
+++ programs/number_increase.py
@@ -0,0 +1,16 @@
+#number_list[10,15,20]
+number_list = []
+for i in range(10, 51, 5):
+    number_list.append(i)
+
+print(f'{number_list=}')
+
+increase_num = int(input("Enter a number to increase by: "))
+
+#my brain hurts today
+number_list_new = []
+for num in number_list:
+    num += increase_num
+    number_list_new.append(num)
+
+print(f'{number_list_new=}')
