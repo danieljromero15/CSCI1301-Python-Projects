79471675cf8747b26ff5c84f5a166e734b087e1f
Daniel Romero <danieljromero305@gmail.com>
2022-11-29 09:52:27 -0500

    Created car_speeds_class_random

---
+++ projects/car_speeds_class_random.py
@@ -0,0 +1,6 @@
+# Title: car_speeds_class_random.py
+# Student name: Daniel Romero
+# Class: CSCI 1301 – Section HY8
+# Professor: Dr. Abi Salimi
+# Purpose: Track the speeds of different cars using classes and objects. The car speeds will be generated randomly.
+
