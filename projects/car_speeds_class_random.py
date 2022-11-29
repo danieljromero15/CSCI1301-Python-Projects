5735b4b0ba503ca50a4f202992eadf56777100ef
Daniel Romero <danieljromero305@gmail.com>
2022-11-29 09:54:01 -0500

    modified car_speeds_class_random

--- projects/car_speeds_class_random.py
+++ projects/car_speeds_class_random.py
@@ -4,3 +4,7 @@
 # Professor: Dr. Abi Salimi
 # Purpose: Track the speeds of different cars using classes and objects. The car speeds will be generated randomly.
 
+class Car:
+    def __init__(self, id, topspeed):
+        return 0
+
