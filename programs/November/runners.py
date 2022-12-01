9e5c71a5dc280a1c6bb5534d6f17f7e33df99d04
Daniel Romero <danieljromero305@gmail.com>
2022-12-01 10:33:56 -0500

    Formatted

--- programs/November/runners.py
+++ programs/November/runners.py
@@ -1,31 +1,46 @@
 from random import randint
 
+
 class Runner:
+
     def __init__(self, id, distance=0):
         self.id = id
         self.distance = distance
-        #self.distance = randint(10, 30) * 5 #5 is hours, we can make that a variable if we so choose
+
+        # self.distance = randint(10, 30) * 5 #5 is hours, we can make that a variable if we so choose
+
         self.speed = randint(10, 30)
 
     def __str__(self):
+
         return f'Runner {self.id} ran {self.distance} miles'
 
-    def distanceCalc(self, hour):
-        self.distance = self.speed * hour
+    def addDistance(self, hour):
+
+        self.distance += self.speed * hour
+
 
 runnersList = []
+
 for i in range(0, 10):
+
     runner = Runner(1000 + i)
-    #print(runner.distance)
+
+    # print(runner.distance)
+
     runnersList.append(runner)
 
-#print(runnersList)
 
-for runner in runnersList:
-    print(runner)
+# print(runnersList)
+
+
+for n in runnersList:
+    print(n)
+
 
 hours = int(input("How many hours long is the race? "))
 
+
 for runner in runnersList:
-    runner.distanceCalc(hours)
+    runner.addDistance(hours)
     print(runner)
